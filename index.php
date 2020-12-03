b6b454989ddd84199e5b8951b7a80f949dce593b
gillout <gillout@bbox.fr>
2020-12-03 18:37:51 +0100

    Refact : Modification de l'accès à la base de données à l'aide d'un fichier config

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once 'config' . DIRECTORY_SEPARATOR . 'config.php';
 
-use Config\MyPDO;
 use Controller\RssController;
 
-$db = new MyPDO();
+$sgbdHost = CONFIG['db.driver'] . ':host=' . CONFIG['db.host'] . ';dbname=' . CONFIG['db.name'] . ';charset=UTF8';
+$db = new PDO($sgbdHost, CONFIG['db.user'], CONFIG['db.password'], CONFIG['pdo.config']);
 
 $ctrl = new RssController($db);
 $ctrl->index();
